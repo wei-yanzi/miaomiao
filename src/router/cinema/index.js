6b0cff3128c5d5b309d2f4abdd297597fdce9d09
wei-yanzi <1591787044@qq.com>
2019-10-29 10:24:12 +0800

    miaomiao-2

--- src/router/cinema/index.js
+++ src/router/cinema/index.js
@@ -1,5 +1,4 @@
 export default {
     path:"/cinema",
-    name:"cinema",
     component:()=>import('@/views/Cinema')
 }
--- src/router/mine/index.js
+++ src/router/mine/index.js
@@ -1,5 +1,4 @@
 export default {
     path:"/mine",
-    name:"mine",
     component:()=>import('@/views/Mine')
 }
